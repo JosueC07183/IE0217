2e66e8c1c862ab473f94f2d43b14b68dcf10a33b
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-04-04 21:19:55 -0600

    Llamado de funciones optimizado

--- campus.cpp
+++ campus.cpp
@@ -12,17 +12,29 @@ campus::campus(string name_Campus, int cantidad_Fincas, string name_Finca){
     
 }
 
+void campus::setName_Sede(string name_Campus){
+    this->name_Campus = name_Campus;
+}
+
 string campus::getName_Sede(){
     return name_Campus;
+}
+
+void campus::setCantidad(int cantidad_Fincas){
+    this->cantidad_Fincas=cantidad_Fincas;
+
 }
 
 int campus::getCantidad(){
     return cantidad_Fincas;
 }
 
-void campus::setCantidad(int cantidad_Fincas){
-    this->cantidad_Fincas=cantidad_Fincas;
+void campus::setName_Finca(string name_Finca){
+    this->name_Finca = name_Finca;
+}
 
+string campus::getName_Finca(){
+    return name_Finca;
 }
 
 string campus::getName(){
