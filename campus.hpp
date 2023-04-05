564bc37dec9bb6b53780aee7a32d8da3f4ca6d59
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-04-04 21:19:17 -0600

    Actualización declaración de funciones.

--- campus.hpp
+++ campus.hpp
@@ -16,8 +16,18 @@ private:
 public:
     campus(string name_Campus, int cantidad_Fincas, string name_Finca);
     ~campus();
+
+    void setName_Sede(string name_Campus);
     string getName_Sede();
-    int getCantidad();
+
     void setCantidad(int cantidad_Fincas);
+    int getCantidad();
+    
+    void setName_Finca(string name_Finca);
+    string getName_Finca();
+
     string getName();
+campus(){
+    
+}
 };
