d5a451e44e704f0ef8773c2d4e44ba2051a4ad76
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-04-04 21:18:35 -0600

    Actualización del main

--- main.cpp
+++ main.cpp
@@ -8,16 +8,17 @@ using namespace std;
 
 int main(){
 
-    campus c1("Sede Rodrigo Facio", 4, "Finca Aire");
-    campus c2("Sede Rodrigo Facio",4, "Finca Viento");
-    campus c3("Sede Rodrigo Facio",4, "Finca Fuego");
-    campus c4("Sede Rodrigo Facio",4, "Finca Agua");
-    cout<<"Campus: "<<c1.getName_Sede()<<". Cantidad de fincas: "<<c1.getCantidad()<<endl;
-    cout<<"Nombre de las fincas: "<<c1.getName()<<endl;
-    cout<<"Nombre de las fincas: "<<c2.getName()<<endl;
-    cout<<"Nombre de las fincas: "<<c3.getName()<<endl;
-    cout<<"Nombre de las fincas: "<<c4.getName()<<endl;
-    
+    campus temp, temp2, temp3, temp4;
+
+    temp.setCantidad(4);
+    temp.setName_Sede("Sede Rodrigo Facio");
+    temp.setName_Finca("Finca Aire");
+    temp2.setName_Finca("Finca Viento");
+    temp3.setName_Finca("Finca Fuego");
+    temp4.setName_Finca("Finca Agua");
+    cout<<"Campus: "<< temp.getName_Sede() <<". Cantidad de fincas: "<< temp.getCantidad() << "\n"<<
+    "Nombre de las fincas: "<<"\n"<<temp.getName_Finca() <<"\n"<<temp2.getName_Finca()<<"\n"<<temp3.getName_Finca()<<"\n"<<temp4.getName_Finca()<<endl;
+
 
     return 0;
 }
