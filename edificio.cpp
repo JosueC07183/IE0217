f017de4219bfdb055aaa8a9de1d8c1c1d0b115c7
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-04-06 22:49:10 -0600

    Se agregan nuevos constructores clase Aulas

--- edificio.cpp
+++ edificio.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #include "edificio.hpp"
 
-
 //Constructor de la clase padre.
 edificio::edificio(int pisos, bool ascensor, string ubicacion){
     this->pisos=pisos;
@@ -39,6 +38,36 @@ void edificio_Parqueo::set_SodaDisponible(string Soda){
 string edificio_Parqueo::get_SodaDisponible(){
     return Soda;
 }
+
+// Constructor clase Aulas.
+void Aulas::set_Sillas(int sillas){
+    this->sillas=sillas;
+}
+int Aulas::get_Sillas(){
+    return sillas;
+}
+
+void Aulas::set_Proyector(string proyector){
+    this->proyector=proyector;
+
+}
+string Aulas::get_Proyector(){
+    return proyector;
+}
+void Aulas::set_Cafeteria(string cafeteria){
+    this->cafeteria=cafeteria;
+}
+string Aulas::get_Cafeteria(){
+    return cafeteria;
+}
+void Aulas::set_NumAula(int num_Aula){
+    this->num_Aula=num_Aula;
+}
+int Aulas::get_NumAula(){
+    return num_Aula;
+}
+
+
 edificio::~edificio()
 {
 }
