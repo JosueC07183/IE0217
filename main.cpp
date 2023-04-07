288b7fb74cbb86dcdcda1ee9d3a7846ca27b85e8
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-04-06 22:49:39 -0600

    Actualización main

--- main.cpp
+++ main.cpp
@@ -2,23 +2,26 @@
 * Función main. Aquí se llaman las funciones 
 * creadas hasta el momento.
 */
+
 #include <iostream>
 #include "campus.hpp"
 #include "finca.hpp"
 #include "edificio.hpp"
-#include "edificio_Aulas.hpp"
 //#include "edificio_Parqueo.hpp"
+//#include "edificio_Aulas.hpp"
+
 using namespace std;
 
 int main(){
     
     //bool bus_Service, *val_Service;// Variable para comprobar el transito del bus interno
     
-    campus temp, temp2, temp3, temp4; // Variables para clase campus
-    finca  aux, aux2, aux3, aux4; // Variables para clase finca
-    //edificio build, build2;// Variables para clase edificio
-// Variables para clase edificio_Parqueo heredada de clase edificio
-    edificio_Parqueo Build, Build2; 
+    campus temp, temp2, temp3, temp4; // Variables para clase campus.
+    finca  aux, aux2, aux3, aux4; // Variables para clase finca.
+// Variables para clase edificio_Parqueo heredada de clase edificio.
+    edificio_Parqueo Build, Build2;
+// Variables para clase Aulas heredada de clase edificio.
+    Aulas Build3, Build4, Build5, Build6, Build7; 
 
     
     
@@ -59,9 +62,9 @@ int main(){
         return 0;
     }*/
     
-//Info general de fincas:
+cout<<"Info general de fincas:"<<endl;
 
-    /*Info Finca Aire*/
+    cout<<"Info Finca Aire"<<endl;
     aux.setNum_Edificio(7);
     aux.setNum_Edificio_Park(2);
     aux.setNum_Edificio_Room(5);
@@ -69,7 +72,7 @@ int main(){
     cout<<temp.getName_Finca() << " posee: "<<aux.getNum_Edificio()<< " edificios, "<<aux.getNum_Edificio_Park()<<
     " son de parqueo y "<<aux.getNum_Edificio_Room()<<" de aulas."<< " Pasa el bus interno? "<< aux.getbus_Service() <<endl;
 
-    /*Info Finca Viento*/
+    cout<<"Info Finca Viento"<<endl;
     aux2.setNum_Edificio(4);
     aux2.setNum_Edificio_Park(1);
     aux2.setNum_Edificio_Room(3);
@@ -77,7 +80,7 @@ int main(){
     cout<< temp2.getName_Finca()<<" posee: "<<aux2.getNum_Edificio()<< " edificios, "<<aux2.getNum_Edificio_Park()<<
     " son de parqueo y "<<aux2.getNum_Edificio_Room()<<" de aulas."<< " Pasa el bus interno? "<< aux2.getbus_Service()<<endl;
 
-    /*Info Finca Fuego*/
+    cout<<"Info Finca Fuego"<<endl;
     aux3.setNum_Edificio(3);
     aux3.setNum_Edificio_Park(1);
     aux3.setNum_Edificio_Room(2);
@@ -85,7 +88,7 @@ int main(){
     cout<< temp3.getName_Finca() <<" posee: "<<aux3.getNum_Edificio()<< " edificios, "<<aux3.getNum_Edificio_Park()<<
     " son de parqueo y "<<aux3.getNum_Edificio_Room()<<" de aulas."<<" Pasa el bus interno? "<<aux3.getbus_Service()<<endl;
 
-    /*Info Finca Agua*/
+    cout<<"Info Finca Agua"<<endl;
     aux4.setNum_Edificio(9);
     aux4.setNum_Edificio_Park(3);
     aux4.setNum_Edificio_Room(6);
@@ -93,11 +96,10 @@ int main(){
     cout<< temp4.getName_Finca()<<" posee: "<<aux4.getNum_Edificio()<< " edificios, "<<aux4.getNum_Edificio_Park()<<
     " son de parqueo y "<<aux4.getNum_Edificio_Room()<<" de aulas."<<" Pasa el bus interno? "<< aux4.getbus_Service() <<endl;
 
-// Detalles de los edificios de parqueo
-    /*Finca Aire: edificios parqueo*/
-    /*Información primer edificio A.1*/
-    //build.setPisos(4);
-    //build.setAscensor(false);
+cout<<"Detalles de los edificios de parqueo."<<endl;
+
+cout<<"************Finca Aire: edificios parqueo************"<<endl;
+    cout<<"*Información edificio A.1*"<<endl;
     Build.setPisos(4);
     Build.setAscensor(false);
     Build.setUbicacion("Costado sureste edificio Ciencias Sociales.");
@@ -105,14 +107,101 @@ int main(){
     cout<<"Edificio A1 posee: "<<Build.getPisos()<< " pisos. "<< "Tiene ascensor? "<<Build.getAscensor()
     <<". Esta ubicado en: "<<Build.getUbicacion()<< " Tiene soda ? "<< Build.get_SodaDisponible() <<endl;
     
-
-    
-    /*Información edificio A.2*/
+    cout<<"*Información edificio A.1*"<<endl;
     Build2.setPisos(6);
     Build2.setAscensor(true);
     Build2.setUbicacion("Diagonal Edificio CELEQ");
     Build2.set_SodaDisponible("Como es un parqueo no tiene soda");
     cout<<"Edificio A2 posee: "<<Build2.getPisos()<< " pisos. "<< "Tiene ascensor? "<<Build2.getAscensor()
     <<". Esta ubicado en: "<<Build2.getUbicacion()<< " Tiene soda ? "<< Build2.get_SodaDisponible() <<endl;
+cout<<"************Finca Aire: edificios aula************"<<endl;
+    cout<<"Información A.3"<<endl;
+    Build3.setPisos(5);
+    Build3.setAscensor("Si");
+    Build3.setUbicacion("Costado oeste Laname");
+    Build3.set_Cafeteria("Si");
+    Build3.set_NumAula(30);
+    Build3.set_Sillas(20);
+    Build3.set_Proyector("Si");
+    cout<<"Aquí la información del edificio A3."<<endl;
+    cout<<"Edificio A3 posee: "<<Build3.getPisos()<< " pisos"<<endl;
+    cout<<"Tiene ascensor ?: "<<Build3.getAscensor()<<endl;
+    cout<<"Ubicación: "<<Build3.getUbicacion()<<endl;
+    cout<<"Tiene cafetería ?: "<<Build3.get_Cafeteria()<<endl;
+    cout<<"Tiene: "<<Build3.get_NumAula()<< " aulas" <<endl;
+    cout<<"Cada aula tiene: "<<Build3.get_Sillas()<< " sillas" <<endl;
+    cout<<"Y las aulas tienen proyector ?: "<<Build3.get_Proyector()<<endl;
+    
+    cout<<"Información A.4"<<endl;
+    Build4.setPisos(3);
+    Build4.setAscensor("No");
+    Build4.setUbicacion("Frente a laboratorio de Docencia");
+    Build4.set_Cafeteria("Si");
+    Build4.set_NumAula(20);
+    Build4.set_Sillas(20);
+    Build4.set_Proyector("Si");
+    cout<<"Aquí la información del edificio A4."<<endl;
+    cout<<"Edificio A4 posee: "<<Build4.getPisos()<< " pisos"<<endl;
+    cout<<"Tiene ascensor ?: "<<Build4.getAscensor()<<endl;
+    cout<<"Ubicación: "<<Build4.getUbicacion()<<endl;
+    cout<<"Tiene cafetería ?: "<<Build4.get_Cafeteria()<<endl;
+    cout<<"Tiene: "<<Build4.get_NumAula()<< " aulas" <<endl;
+    cout<<"Cada aula tiene: "<<Build4.get_Sillas()<< " sillas" <<endl;
+    cout<<"Y las aulas tienen proyector ?: "<<Build4.get_Proyector()<<endl;
+    
+    cout<<"Información A.5"<<endl;
+    Build5.setPisos(6);
+    Build5.setAscensor("Si");
+    Build5.setUbicacion("Frente al Planetario");
+    Build5.set_Cafeteria("Si");
+    Build5.set_NumAula(70);
+    Build5.set_Sillas(20);
+    Build5.set_Proyector("Si");
+    cout<<"Aquí la información del edificio A5."<<endl;
+    cout<<"Edificio A5 posee: "<<Build5.getPisos()<< " pisos"<<endl;
+    cout<<"Tiene ascensor ?: "<<Build5.getAscensor()<<endl;
+    cout<<"Ubicación: "<<Build5.getUbicacion()<<endl;
+    cout<<"Tiene cafetería ?: "<<Build5.get_Cafeteria()<<endl;
+    cout<<"Tiene: "<<Build5.get_NumAula()<< " aulas" <<endl;
+    cout<<"Cada aula tiene: "<<Build5.get_Sillas()<< " sillas" <<endl;
+    cout<<"Y las aulas tienen proyector ?: "<<Build5.get_Proyector()<<endl;
+
+    cout<<"Información A.6"<<endl;
+    Build6.setPisos(4);
+    Build6.setAscensor("No");
+    Build6.setUbicacion("100 mts norte del Planetario");
+    Build6.set_Cafeteria("No");
+    Build6.set_NumAula(20);
+    Build6.set_Sillas(15);
+    Build6.set_Proyector("Si");
+    cout<<"Aquí la información del edificio A6."<<endl;
+    cout<<"Edificio A6 posee: "<<Build6.getPisos()<< " pisos"<<endl;
+    cout<<"Tiene ascensor ?: "<<Build6.getAscensor()<<endl;
+    cout<<"Ubicación: "<<Build6.getUbicacion()<<endl;
+    cout<<"Tiene cafetería ?: "<<Build6.get_Cafeteria()<<endl;
+    cout<<"Tiene: "<<Build6.get_NumAula()<< " aulas" <<endl;
+    cout<<"Cada aula tiene: "<<Build6.get_Sillas()<< " sillas" <<endl;
+    cout<<"Y las aulas tienen proyector ?: "<<Build6.get_Proyector()<<endl;
+
+    cout<<" -----------------"<<endl;
+    cout<<" |Información A.7|"<<endl;
+    cout<<" -----------------"<<endl;
+    Build7.setPisos(3);
+    Build7.setAscensor("No");
+    Build7.setUbicacion("Frente al edificio de Ingeniería");
+    Build7.set_Cafeteria("Si");
+    Build7.set_NumAula(25);
+    Build7.set_Sillas(18);
+    Build7.set_Proyector("Si");
+    cout<<"Aquí la información del edificio A7."<<endl;
+    cout<<"Edificio A7 posee: "<<Build7.getPisos()<< " pisos"<<endl;
+    cout<<"Tiene ascensor ?: "<<Build7.getAscensor()<<endl;
+    cout<<"Ubicación: "<<Build7.getUbicacion()<<endl;
+    cout<<"Tiene cafetería ?: "<<Build7.get_Cafeteria()<<endl;
+    cout<<"Tiene: "<<Build7.get_NumAula()<< " aulas" <<endl;
+    cout<<"Cada aula tiene: "<<Build7.get_Sillas()<< " sillas" <<endl;
+    cout<<"Y las aulas tienen proyector ?: "<<Build7.get_Proyector()<<endl;
+
+
     return 0;
 }
