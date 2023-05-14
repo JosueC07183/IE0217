664db6e412b06c1eaebd40334b5c30c2a1b07b20
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-05-14 12:08:24 -0600

    Creación de atributos

---
+++ Tarea3/Empleado.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Empleado
+{
+private:
+    /* data */
+public:
+
+    string nombre;
+    string correo;
+    int edad;
+    string departamento;
+    double salario;
+
+    Empleado(string nombre, string correo, int edad, string departamento, double salario);
+    ~Empleado();
+
+    void setNombre(string nombre);
+    string getNombre();
+
+    void setCorreo(string correo);
+    string getCorreo();
+
+    void setEdad(int edad);
+    int getEdad();
+
+    void setDepartamento(string departamento);
+    string getDepartamento();
+
+    void setSalario(double salario);
+    double getSalario();
+
+
+};
+
