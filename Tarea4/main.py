f232e4c1ddd8062dd439d5fbefbc381b4904797c
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-06-04 12:59:50 -0600

    Construyendo main

--- Tarea4/main.py
+++ Tarea4/main.py
@@ -1,22 +1,21 @@
-def leerArchivo():
-    aux = input("Escriba la dirección del archivo\n")
-    with open(aux, "r") as file:
-        info = file.read()
-    print(info)
-    file.close()
 
 
 while True:
     print("Menú principal")
-    print("1. Leer archivo")
-    print("2. Buscar por departamento")
-    print("3. Buscar por salario")
-    print("4. Salir")
+    print("0. Salir")
+    print("1. Consultar por departamento")
+    print("2. Consultar por salario")
     opcion = input("Ingrese una opción\n")
-    if opcion == "1":
-        leerArchivo()  # Poner excepción aqui.
-    elif opcion == "4":
+    if opcion == "0":
         exit()
+    elif opcion == "1":
+        archivo_principal = "../../employees.csv" # Archivo por default para calificar
+        departamento = input("Escribe el departamento que desea buscar\n")
+    elif opcion == "2":
+        archivo_principal = "../../employees.csv" # Archivo por default para calificar
+        sal_Min = int(input("Digite el salario mínimo: "))
+        sal_Max = int(input("Digite el salario máximo: "))
+        
 
     else:
         print("opcion inválida \n")
