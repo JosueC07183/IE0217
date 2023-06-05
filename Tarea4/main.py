84586525ff3d5fe99ce39fe3238bc4583aeb618f
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-06-04 21:42:46 -0600

    Agregando excepciones al main

--- Tarea4/main.py
+++ Tarea4/main.py
@@ -14,9 +14,29 @@ while True:
         Empleado.buscarPorDepartamento(archivo_principal, departamento)
     elif opcion == "2":
         archivo_principal = "../../employees.csv" # Archivo por default para calificar
-        sal_Min = int(input("Digite el salario mínimo: "))
-        sal_Max = int(input("Digite el salario máximo: "))
-        
-
+        while True:
+            try:
+                sal_Min = int(input("Digite el salario mínimo: "))
+                if sal_Min < 0:
+                    print("El salario mínimo debe ser un número positivo")
+                    continue
+                sal_Max = int(input("Digite el salario máximo: "))
+                if sal_Max < 0:
+                    print("El salario máximo debe ser un número positivo")
+                    continue
+                elif sal_Min > sal_Max:
+                    print("Error al digitar el orden del intervalo solicitado.\n")
+                    continue
+                break
+            except ValueError:
+                print("Al menos uno de los rangos digitados no es válido\n")
+                continue
+        intervalo_Salario = Empleado.buscarPorSalario(archivo_principal, sal_Min, sal_Max)
+        for empleado in intervalo_Salario:
+            print(f"Nombre: {empleado[0].strip()}")
+            print(f"Correo: {empleado[1].strip()}")
+            print(f"Edad: {empleado[2].strip()}")
+            print(f"Departamento: {empleado[4].strip()}")
+            print("\n") # Se muestre un salto de línea por cada empleado.
     else:
         print("opcion inválida \n")
