4089eb58c8be36ea7c403f646e68e8ce3ec67c6b
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-06-05 23:16:07 -0600

    Update main

--- Tarea4/main.py
+++ Tarea4/main.py
@@ -9,11 +9,11 @@ while True:
     if opcion == "0":
         exit()
     elif opcion == "1":
-        archivo_principal = "../../employees.csv" # Archivo por default para calificar
+        archivo_principal = "../../Datos.csv"
         departamento = input("Escribe el departamento que desea buscar\n")
         Empleado.buscarPorDepartamento(archivo_principal, departamento)
     elif opcion == "2":
-        archivo_principal = "../../employees.csv" # Archivo por default para calificar
+        archivo_principal = "../../Datos.csv"
         while True:
             try:
                 sal_Min = int(input("Digite el salario mínimo: "))
@@ -25,20 +25,14 @@ while True:
                     print("El salario máximo debe ser un número positivo")
                     continue
                 elif sal_Min > sal_Max:
-                    print("Error al digitar el orden del intervalo solicitado.\n")
+                    print("Error en el orden del intervalo solicitado.\n")
                     continue
                 break
             except ValueError:
                 print("Al menos uno de los rangos digitados no es válido\n")
                 continue
-        intervalo_Salario = Empleado.buscarPorSalario(archivo_principal, sal_Min, sal_Max)
-        for empleado in intervalo_Salario:
-            correo = empleado[1].strip()
-            print(f"Nombre: {empleado[0].strip()}")
-            #print(f"Correo: {empleado[1].strip()}")
-            print(f"Correo: {correo}")
-            print(f"Edad: {empleado[2].strip()}")
-            print(f"Departamento: {empleado[4].strip()}")
-            print("\n") # Se muestre un salto de línea por cada empleado.
+        intervalo_Salario = Empleado.buscarPorSalario(
+            archivo_principal, sal_Min, sal_Max
+        )
     else:
         print("opcion inválida \n")
