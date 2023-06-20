a01258586ac2a0e3ccd81a58b10abe399525e76e
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-06-20 14:42:14 -0600

    Editando main

--- Tarea5/main.py
+++ Tarea5/main.py
@@ -1,10 +1,34 @@
+# Inicialmente de la carpeta Functions-
+# se importa el archivo funciones.
 from Functions import funciones
 
-# Análisis de datos con generación neta: Gas natural
-mostrar_Info = funciones.llamado_datos()
-orden_Info = funciones.orden_Datos()
-bars = funciones.exp_analysis()
-CoefPearson = funciones.pearson_Val('Electricity Net Generation From Natural Gas, All Sectors', 'Electricity Net Generation From Geothermal, All Sectors')
+# Llamado de los datos de la página web dada.
+llamar_datos = funciones.llamado_datos()
 
-# Análisis de datos con generación neta: Carbón.
-mostrar_Info_2 = funciones.pie_Analysis()
+# Ordenanamiento del csv
+ordenar_csv = funciones.orden_datos()
+
+# Se hace uso de la primera función para comprobar-
+# el manejo de excepciones.
+prueba = funciones.stats_energy('1990', 'Electricity Net Generation From Water, All Sectors')
+
+# Análisis tendencia exponencial con los datos de-
+# generación neta: Gas natural.
+bars = funciones.exp_analysis()
+
+# esto genera un error. Pero aquí se puede hace el catch, luego,-
+# realizar un ciclo for que vaya iterando los años, y muestre el-
+#  maximo y el minimo.
+CoefPearson = funciones.pearson_val(
+    "Electricity Net Generation From Natural Gas, All Sectors",
+    "Electricity Net Generation From Solar, All Sectors",
+ )
+
+# Análisis de datos con generación neta: Carbón, por medio de un-
+# diagrama de pastel de donde se muestra la desviación estándar.
+std_value_pieanalysis = funciones.pie_analysis()
+
+# Análisis de datos con generación neta: de otros sectores no-
+# considerados por medio de un diagrama de dispersión.
+info_scatter = funciones.scatter_analysis()
+
