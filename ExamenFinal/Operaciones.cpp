ed1c83e2c58c791dab75b7131f9c20aff908d248
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-07-10 19:59:14 -0600

    Ejercicio practico 1, listo

---
+++ ExamenFinal/Operaciones.cpp
@@ -0,0 +1,38 @@
+#include "Operaciones.hpp"
+#include <iostream>
+using namespace std;
+
+ops::ops(float num_1, float num_2)
+{
+    x = num_1;
+    y = num_2;
+}
+
+void ops::Resultado()
+{
+    
+    cout << "La suma de: " << x << " y "<< y << " es:" <<Suma() << endl;
+    cout << "La suma de: " << x << " y "<< y << " es:" <<Resta() << endl;
+    cout << "La suma de: " << x << " y "<< y << " es:" <<Producto() << endl;
+    cout << "La suma de: " << x << " y "<< y << " es:" <<Division() << endl;
+}
+
+float ops::Suma()
+{
+    return x + y;
+}
+
+float ops::Resta()
+{
+    return x - y;
+}
+
+float ops::Producto()
+{
+    return x * y;
+}
+
+float ops::Division()
+{
+    return x / y;
+}
