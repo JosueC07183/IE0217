ce19dda05c5f7308d965162b1952048db21f0c1f
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-07-10 20:00:50 -0600

    Moviendo archivos ejercicio 1

---
+++ ExamenFinal/Ejercicio1/Operaciones.hpp
@@ -0,0 +1,19 @@
+class ops
+{
+private:
+    float x;
+    float y;
+
+public:
+    ops(float num_1, float num_2);
+
+    void Resultado();
+
+    float Suma();
+
+    float Resta();
+
+    float Producto();
+
+    float Division();
+};
---
+++ ExamenFinal/Ejercicio1/main.cpp
@@ -0,0 +1,11 @@
+#include "Operaciones.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ops Valores(4, 5);
+    Valores.Resultado();
+
+    return 0;
+}
